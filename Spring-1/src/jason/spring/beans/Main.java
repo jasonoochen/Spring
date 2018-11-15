064da54dd51cca1a35e620887c97c643739f6785
Jiecheng Chen <chjchjason@gmail.com>
2018-11-15 12:15:49 -0800

    update

---
+++ Spring-1/src/jason/spring/beans/Main.java
@@ -0,0 +1,11 @@
+package jason.spring.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.setName("jason");
+		helloWorld.hello();
+	}
+
+}
