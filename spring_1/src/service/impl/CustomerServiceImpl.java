00fc48dd53c15ddc38259f0d3d5e817bdb93fc0c
Jiecheng Chen <chjchjason@gmail.com>
2019-02-03 15:01:57 -0800

    configuration

---
+++ spring_1/src/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package service.impl;
+
+import dao.ICustomerDao;
+import service.ICustomerService;
+
+/*
+ * 客户的业务层实现类
+ */
+
+public class CustomerServiceImpl implements ICustomerService {
+	
+//	private ICustomerDao customerDao = new CustomerDaoImpl();
+	
+//	private ICustomerDao customerDao = BeanFactory.getCustomerDao();
+	
+	private ICustomerDao customerDao = null;
+	
+	@Override
+	public void saveCustomer() {
+		System.out.println("业务层调用持久层");
+		customerDao.saveCustomer();
+	}
+
+}
---
+++ spring_1/src/ui/Client.java
@@ -0,0 +1,25 @@
+package ui;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dao.ICustomerDao;
+import service.ICustomerService;
+
+public class Client {
+
+	/*
+	 * ClassPathXmlApplicationContext:它只能加载类路径下的配置文件
+	 * FileSystemXmlApplicationContext:它可以加载磁盘任意位置的配置文件
+	 */
+	public static void main(String[] args) {
+		//1.获取容器
+		ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+		//2.根据bean的id获取对象
+		ICustomerService cs = (ICustomerService) ac.getBean("customerService");
+		ICustomerDao cdao = (ICustomerDao) ac.getBean("customerDao");
+		System.out.println(cs);
+		System.out.println(cdao);
+	}
+
+}
