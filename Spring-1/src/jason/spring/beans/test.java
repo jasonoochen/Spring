0b4a0401cffd606d47ff437c328e7bfb8d21ec9e
Jiecheng Chen <chjchjason@gmail.com>
2019-02-02 18:46:49 -0800

    v1

---
+++ Spring-1/src/jason/spring/beans/test.java
@@ -0,0 +1,18 @@
+package jason.spring.beans;
+
+import java.util.*;
+
+public class test {
+	
+	public static void main(String[] args) {
+		int i = 4;
+		List<String> li = help(i);
+		for(String s : li)
+			System.out.println(s);
+	}
+
+	private static List<String> help(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+}
