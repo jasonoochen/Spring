699a5763f7d96db318a3227ac5add3e4d4ad077f
Jiecheng Chen <chjchjason@gmail.com>
2019-02-05 22:38:29 -0800

    annotation_bean

--- spring_4_annotation_IOC/src/service/impl/CustomerServiceImpl.java
+++ spring_4_annotation_IOC/src/service/impl/CustomerServiceImpl.java
@@ -1,17 +1,23 @@
 package service.impl;
 
+import org.springframework.stereotype.Component;
+
 import dao.ICustomerDao;
 import service.ICustomerService;
 
 /*
  * 客户的业务层实现类
+ * 
+ * 注解
+ * 1.用于创建bean对象
+ * 		@Component
+ * 			作用：配置一个bean标签
+ * 			出现位置：class上方
+ * 			属性：value.含义是指定bean的id。当不写时，默认值是当前类的短名，首字母改小写
  */
 
+@Component
 public class CustomerServiceImpl implements ICustomerService {
-	
-//	private ICustomerDao customerDao = new CustomerDaoImpl();
-	
-//	private ICustomerDao customerDao = BeanFactory.getCustomerDao();
 	
 	private ICustomerDao customerDao = null;
 
---
+++ spring_4_annotation_IOC/src/ui/Client.java
@@ -0,0 +1,16 @@
+package ui;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dao.ICustomerDao;
+import service.ICustomerService;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+		ICustomerService cs = (ICustomerService) ac.getBean("customerServiceImpl");
+		cs.saveCustomer();
+	}
+}
